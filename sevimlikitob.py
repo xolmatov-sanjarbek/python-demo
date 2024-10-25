019756832b32a6b8768ae0410123188f145e6d54
Xolmatov Sanjarbek <xsanjarbek725@gmail.com>
2024-10-25 19:08:55 +0300

    Add files via upload

---
+++ sevimlikitob.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 25 20:50:34 2024
+
+@author: user
+"""
+
+savol = "Sevimli kitobingizning nomini yozing "
+savol += "(dasturni to'xtatish uchun 'stop' deb yozing') "
+
+while True:
+    kitob = input(savol)
+    if kitob == 'stop':
+        break
+    
+    
+    
